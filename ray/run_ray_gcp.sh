62ed98d9e7903d1aa8ab9fe18cc632f6c584684a
Ayat Aboalnaga <ayat.aboalnaga@gmail.com>
2025-07-22 21:15:22 +0300

    ray cluster config & script (gcp)

---
+++ ray/run_ray_gcp.sh
@@ -0,0 +1,8 @@
+# install gcloud
+curl https://sdk.cloud.google.com | bash
+
+export GOOGLE_PROJECT_ID=be-loo-prototyping
+# login to gcloud
+gcloud init
+
+gcloud config set project $GOOGLE_PROJECT_ID
